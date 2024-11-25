05258f14daa619dd3813d4bf8092350e29ef858b
Nivedhika <ashalanivedhika0812@gmail.com>
2024-11-25 15:39:32 +0530

    java test

---
+++ Array.java
@@ -0,0 +1,7 @@
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 1, 2};
+        Arrays.sort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
